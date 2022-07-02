efebe9f4a9f2982c36c56990abe3cbac4a3c40c3
sohom dey <sohomjuit@gmail.com>
2022-07-02 20:10:59 +0530

    download page done

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const connectDB=require('./config/db');
 const express=require("express");
-
+const path=require("path");
 
 const app=express();
-
+app.use(express.static('public'));
 connectDB();
-
-
+//template engine
+app.set('views',path.join(__dirname,'/views'))
+app.set('view engine','ejs');
 //routes
 app.use('/api/files',require('./routes/files'));
+app.use('/files',require('./routes/show'));
 //port connection
 const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
